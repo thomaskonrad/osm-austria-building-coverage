16734ae621cd9505892e7b2ae9f21e4d6680d38c
Thomas Konrad <tkonrad@gmx.net>
2014-09-08 22:12:01 +0200

    Make tile rendering updates faster as we now only scale new tiles

--- scripts/mapnik/scale-tiles-to-lower-zoom-levels.py
+++ scripts/mapnik/scale-tiles-to-lower-zoom-levels.py
@@ -24,6 +24,14 @@ def get_image_or_transparent(path, width=256, height=256):
         return get_transparent_image(width, height)
 
 
+def is_file_newer_than(file1, file2):
+    if os.path.exists(file1) and os.path.exists(file2):
+        return os.path.getctime(file1) > os.path.getctime(file2)
+
+    if not os.path.exists(file1):
+        return False
+
+
 def main():
     if len(sys.argv) < 3:
         print "Usage: ./scale-tiles-to-lower-zoom-levels.py <tiles_path> <starting_zoom_level>"
@@ -69,26 +77,36 @@ def main():
                 nextX = x * 2
                 nextY = y * 2
 
-                nextImages = []
+                next_images = []
 
-                nextImages.append(get_image_or_transparent(path + '%d/%d/%d.png' % (nextZ, nextX, nextY)))
-                nextImages.append(get_image_or_transparent(path + '%d/%d/%d.png' % (nextZ, nextX, nextY + 1)))
-                nextImages.append(get_image_or_transparent(path + '%d/%d/%d.png' % (nextZ, nextX + 1, nextY)))
-                nextImages.append(get_image_or_transparent(path + '%d/%d/%d.png' % (nextZ, nextX + 1, nextY + 1)))
+                image1_path = path + '%d/%d/%d.png' % (nextZ, nextX, nextY)
+                image2_path = path + '%d/%d/%d.png' % (nextZ, nextX, nextY + 1)
+                image3_path = path + '%d/%d/%d.png' % (nextZ, nextX + 1, nextY)
+                image4_path = path + '%d/%d/%d.png' % (nextZ, nextX + 1, nextY + 1)
 
-                for nextImage in nextImages:
-                    nextImage.thumbnail((128, 128), Image.ANTIALIAS)
+                current_path = path + '%d/%d/%d.png' % (z, x, y)
 
-                scaledImage = Image.new('RGBA', (256, 256))
-                scaledImage.paste(nextImages[0], (0, 0))
-                scaledImage.paste(nextImages[1], (0, 128))
-                scaledImage.paste(nextImages[2], (128, 0))
-                scaledImage.paste(nextImages[3], (128, 128))
+                if not os.path.exists(current_path) \
+                        or is_file_newer_than(image1_path, current_path) \
+                        or is_file_newer_than(image2_path, current_path) \
+                        or is_file_newer_than(image3_path, current_path) \
+                        or is_file_newer_than(image4_path, current_path):
+                    next_images.append(get_image_or_transparent(image1_path))
+                    next_images.append(get_image_or_transparent(image2_path))
+                    next_images.append(get_image_or_transparent(image3_path))
+                    next_images.append(get_image_or_transparent(image4_path))
 
-                print('Saving image...')
+                    for next_image in next_images:
+                        next_image.thumbnail((128, 128), Image.ANTIALIAS)
 
-                currentPath = path + '%d/%d/%d.png' % (z, x, y)
-                scaledImage.save(currentPath)
+                    scaled_image = Image.new('RGBA', (256, 256))
+                    scaled_image.paste(next_images[0], (0, 0))
+                    scaled_image.paste(next_images[1], (0, 128))
+                    scaled_image.paste(next_images[2], (128, 0))
+                    scaled_image.paste(next_images[3], (128, 128))
+
+                    print('Image neede renewal and has been created, saving...')
+                    scaled_image.save(current_path)
 
 
 if __name__ == "__main__":main()
--- scripts/mapnik/update-tiles.sh
+++ scripts/mapnik/update-tiles.sh
@@ -27,13 +27,15 @@ echo "$(current_time) Generating tiles..."
 ${DIR}generate_tiles_multiprocess.py ${DIR}osm-buildings-only.xml $tiles_dir $zoom_level $zoom_level > /dev/null
 
 if [ $? -eq 0 ]; then
+	echo "$(current_time) Syncing highest zoom level with current tiles..."
+    rsync -vrtc ${tiles_dir}/${zoom_level} ${tiles_root_dir}${tiles_dir_base_name}-current/${zoom_level}
+    echo "$(current_time) Done!"
+
 	echo "$(current_time) Scaling tiles to lower zoom levels..."
-	${DIR}scale-tiles-to-lower-zoom-levels.py ${tiles_dir}/ $zoom_level > /dev/null
+	${DIR}scale-tiles-to-lower-zoom-levels.py ${tiles_root_dir}${tiles_dir_base_name}-current/ $zoom_level > /dev/null
 
 	if [ $? -eq 0 ]; then
-		echo "$(current_time) Creating symlink..."
-		ln -sfn $tiles_dir ${tiles_root_dir}${tiles_dir_base_name}-current
-		echo "$(current_time) Done!"
+		echo "$(current_time) Scaling tiles done."
 	else
 		echo "$(current_time) Scaling tiles failed."
 	fi
