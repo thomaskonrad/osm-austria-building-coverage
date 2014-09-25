ef5dc93ccd1179ab02ca8af0bc99bcc3737474a5
Thomas Konrad <tkonrad@gmx.net>
2014-09-25 22:00:57 +0200

    Add materialized views (and more) for the coverage score viewer

--- scripts/coverage-scores/create-admin-boundaries.sql
+++ scripts/coverage-scores/create-admin-boundaries.sql
@@ -77,6 +77,8 @@ insert into austria_admin_boundaries (admin_level, name, abbreviation, way, bbox
 
 -- Update GKZ of Vienna (which is both a federal state and a municipality)
 update austria_admin_boundaries set gkz='900' where admin_level = 2 and gkz='9,90001';
+update austria_admin_boundaries set gkz='10101' where admin_level = 3 and name='Eisenstadt (Stadt)';
+update austria_admin_boundaries set gkz='10201' where admin_level = 3 and name='Rust';
 
 -- Insert all municipalities (takes 55 seconds on my machine)
 insert into austria_admin_boundaries (admin_level, name, way, bbox, way_area, gkz, parent)
@@ -103,6 +105,8 @@ insert into austria_admin_boundaries (admin_level, name, way, bbox, way_area, gk
     )
     order by gkz
 );
+
+update austria_admin_boundaries set gkz='61214' where name = 'Großsölk';
 
 -- Insert city districts / municipalities as municipalities
 insert into austria_admin_boundaries (admin_level, name, way, bbox, way_area, gkz, parent)
@@ -179,16 +183,175 @@ ON austria_building_coverage (municipality_id);
 CREATE INDEX idx_austria_building_coverage_timestamp
 ON austria_building_coverage (timestamp);
 
--- Create views
-create or replace view municipality_coverage as
-select b.gkz, b.name as municipality_name, p.id as parent_id, p.name as parenet_name,
-    max(c.timestamp) as latest_timestamp, sum(c.total_pixels) as total_pixels,
-    (sum(c.covered_basemap_pixels)::float / (sum(c.covered_basemap_pixels) + sum(c.uncovered_basemap_pixels)) * 100.0) as coverage
-from austria_building_coverage c
-left join austria_admin_boundaries b on (c.municipality_id = b.id)
+-- Create simplified admin polygons
+
+-- Drop views first
+drop materialized view if exists coverage_boundary;
+drop materialized view if exists coverage_score_base;
+
+drop table if exists simplified_polygon;
+
+create table simplified_polygon
+(
+  id int not null,
+  admin_level int not null,
+  polygon text,
+  bbox text
+);
+
+insert into simplified_polygon(id, admin_level, polygon, bbox)
+
+select b.gkz::int as id, b.admin_level,
+ST_AsGeoJSON(ST_Transform(ST_Collect(ST_Simplify(b.way, 100)), 4326)) as polygon,
+ST_AsGeoJSON(ST_Transform(ST_Envelope(ST_Collect(ST_Simplify(b.way, 100))), 4326)) as bbox
+from austria_admin_boundaries b
+where b.admin_level = 3
+group by b.gkz, b.admin_level
+
+union all
+
+select b.gkz::int as id, b.admin_level,
+ST_AsGeoJSON(ST_Transform(ST_Collect(ST_Simplify(b.way, 500)), 4326)) as polygon,
+ST_AsGeoJSON(ST_Transform(ST_Envelope(ST_Collect(ST_Simplify(b.way, 500))), 4326)) as bbox
+from austria_admin_boundaries b
+where b.admin_level = 2
+group by b.gkz, b.admin_level
+
+union all
+
+select b.gkz::int as id, b.admin_level,
+ST_AsGeoJSON(ST_Transform(ST_Collect(ST_Simplify(b.way, 1000)), 4326)) as polygon,
+ST_AsGeoJSON(ST_Transform(ST_Envelope(ST_Collect(ST_Simplify(b.way, 1000))), 4326)) as bbox
+from austria_admin_boundaries b
+where b.admin_level = 1
+group by b.gkz, b.admin_level
+
+union all
+
+select 0::int as id, 0 as admin_level,
+ST_AsGeoJSON(ST_Transform(ST_Collect(ST_Simplify(p.way, 1500)), 4326)) as polygon,
+ST_AsGeoJSON(ST_Transform(ST_Envelope(ST_Collect(ST_Simplify(p.way, 1500))), 4326)) as bbox
+from planet_osm_polygon p
+where p.admin_level = '2' and p.name='Österreich'
+group by id;
+
+-- Base view
+create materialized view coverage_score_base as
+select b.gkz::int as gkz, b.name as name, p.gkz::int as district_id,
+  max(c_current.timestamp) as latest_timestamp,
+  sum(c_current.total_pixels) as total_pixels,
+  sum(c_current.covered_basemap_pixels) as covered_basemap_pixels,
+  sum(c_current.uncovered_basemap_pixels) as uncovered_basemap_pixels,
+  sum(c_original.covered_basemap_pixels) as covered_basemap_pixels_original,
+  sum(c_original.uncovered_basemap_pixels) as uncovered_basemap_pixels_original
+from austria_building_coverage c_original,
+  austria_building_coverage c_current
+left join austria_admin_boundaries b on (c_current.municipality_id = b.id)
 left join austria_admin_boundaries p on (b.parent = p.id)
-where c.timestamp = (
-    select max(timestamp) from austria_building_coverage c1
-    where c.municipality_id = c1.municipality_id)
-group by b.gkz, b.name, p.id, p.name
-order by coverage desc
+where c_current.timestamp = (
+  select max(timestamp) from austria_building_coverage c1 -- The newest timestamp
+  where c_current.municipality_id = c1.municipality_id)
+and c_original.municipality_id = b.id
+and c_original.timestamp = (
+  select min(timestamp) from austria_building_coverage c2
+  where c_original.municipality_id = c2.municipality_id
+)
+group by b.gkz, b.name, p.gkz;
+
+-- Coverage boundary view
+create materialized view coverage_boundary as
+
+-- Municipalities
+select csb.gkz as id, 3::int as admin_level, csb.name,
+  rank() over(order by (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)  desc) as rank,
+  csb.district_id as parent_id,
+  csb.latest_timestamp,
+  sum(csb.total_pixels) as total_pixels,
+  sum(csb.covered_basemap_pixels) as covered_basemap_pixels,
+  sum(csb.uncovered_basemap_pixels) as uncovered_basemap_pixels,
+  (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0) as coverage,
+  (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0) as original_coverage,
+  (
+    (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)
+    -
+    (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0)
+  ) as total_coverage_gain,
+  s.polygon, s.bbox
+from coverage_score_base csb,
+  simplified_polygon s
+where csb.gkz = s.id and s.admin_level = 3
+group by csb.gkz, csb.name, csb.district_id, csb.latest_timestamp, s.polygon, s.bbox
+
+
+union all
+
+-- Districts
+select b.gkz::int as id, 2::int as admin_level, b.name as name,
+  rank() over(order by (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)  desc) as rank,
+  p.gkz::int as parent_id,
+  max(csb.latest_timestamp) as latest_timestamp,
+  sum(csb.total_pixels) as total_pixels,
+  sum(csb.covered_basemap_pixels) as covered_basemap_pixels,
+  sum(csb.uncovered_basemap_pixels) as uncovered_basemap_pixels,
+  (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0) as coverage,
+  (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0) as original_coverage,
+  (
+    (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)
+    -
+    (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0)
+  ) as total_coverage_gain,
+  s.polygon, s.bbox
+from simplified_polygon s,
+  austria_admin_boundaries b
+  left join austria_admin_boundaries p on (b.parent = p.id)
+  left join coverage_score_base csb on (csb.district_id = b.gkz::int)
+where b.admin_level = 2
+  and b.gkz::int = s.id and s.admin_level = 2
+group by b.gkz::int, b.name, p.gkz, s.polygon, s.bbox
+
+union all
+
+-- States
+select state.gkz::int as id, 1::int as admin_level, state.name,
+  rank() over(order by (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)  desc) as rank,
+  0::int as parent_id,
+  max(csb.latest_timestamp) as latest_timestamp,
+  sum(csb.total_pixels) as total_pixels,
+  sum(csb.covered_basemap_pixels) as covered_basemap_pixels,
+  sum(csb.uncovered_basemap_pixels) as uncovered_basemap_pixels,
+  (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0) as coverage,
+  (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0) as original_coverage,
+  (
+    (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)
+    -
+    (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0)
+  ) as total_coverage_gain,
+  s.polygon, s.bbox
+from simplified_polygon s,
+  austria_admin_boundaries state
+  left join austria_admin_boundaries district on (district.parent = state.id)
+  left join coverage_score_base csb on (csb.district_id = district.gkz::int)
+where state.admin_level = 1
+  and state.gkz::int = s.id and s.admin_level = 1
+group by state.gkz, state.name, s.polygon, s.bbox
+
+union all
+
+-- Countries
+select 0 as id, 0::int as admin_level, 'Österreich'::text as name, 1::int as rank, null as parent_id,
+  max(csb.latest_timestamp) as latest_timestamp,
+  sum(csb.total_pixels) as total_pixels,
+  sum(csb.covered_basemap_pixels) as covered_basemap_pixels,
+  sum(csb.uncovered_basemap_pixels) as uncovered_basemap_pixels,
+  (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0) as coverage,
+  (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0) as original_coverage,
+  (
+    (sum(csb.covered_basemap_pixels)::float / (sum(csb.covered_basemap_pixels) + sum(csb.uncovered_basemap_pixels)) * 100.0)
+    -
+    (sum(csb.covered_basemap_pixels_original)::float / (sum(csb.covered_basemap_pixels_original) + sum(csb.uncovered_basemap_pixels_original)) * 100.0)
+  ) as total_coverage_gain,
+  s.polygon, s.bbox
+from simplified_polygon s,
+  coverage_score_base csb
+where s.id = 0 and s.admin_level = 0
+group by s.polygon, s.bbox
