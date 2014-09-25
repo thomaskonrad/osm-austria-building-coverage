b1ed7129b2c561051fc7aeb120e5f945789bdddf
Thomas Konrad <tkonrad@gmx.net>
2014-09-26 01:15:24 +0200

    Set debug back to false

--- osm_austria_building_coverage/wsgi.py
+++ osm_austria_building_coverage/wsgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 import sys
+
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+except:
+    a = 1
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osm_austria_building_coverage.settings")
 
 from django.core.wsgi import get_wsgi_application
