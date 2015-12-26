217d3153156a47946ff6e69237d3d76d44976d34
Thomas Konrad <mail@thomaskonrad.at>
2015-12-26 10:20:12 +0100

    Update bash Shebang and basemap tile directory.

--- scripts/basemap/basemap-tile-downloader.py
+++ scripts/basemap/basemap-tile-downloader.py
@@ -1,4 +1,4 @@
-#!/opt/local/bin/python3.3
+#!/usr/bin/env python3
 import sys
 import os
 import requests
@@ -25,7 +25,7 @@ print("That's %d tiles total." % (totalNumberOfTiles))
 # Loop through all tiles
 for x in range(minX, maxX + 1):
     for y in range(minY, maxY + 1):
-        directory = "basemap-tiles-%d/%d/" % (zoomLevel, x)
+        directory = "basemap-%d/%d/" % (zoomLevel, x)
         file = directory + "%d.png" % (y)
 
         if not os.path.isfile(file):
